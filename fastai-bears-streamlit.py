173a6f3d0e1be0ece4d76dd35464e61488e69819
Jawaid Hakim <himalayahall@gmail.com>
2023-02-02 20:04:24 -0500

    msg change
    Signed-off-by: Jawaid Hakim <himalayahall@gmail.com>

--- fastai-bears-streamlit.py
+++ fastai-bears-streamlit.py
@@ -96,7 +96,7 @@ class Predict:
             msg_idx = random.randint(0, len(msgs))
             msg = msgs[msg_idx]
 
-            st.write(f'## {msg}: Pred: {pred}; Prob: {prob:.04f}')
+            st.write(f'## {msg}: {pred}; Prob: {prob:.04f}')
 
             if prob >= 0.95 and random.randint(1, 10) > 8: # show balloons 20% of time
                 st.balloons()
